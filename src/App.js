f5d99b6146a2f4b132efd74fe1df59bf390ac369
Chuks <ikedichukwubuikem@gmail.com>
2024-05-08 04:31:20 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import './App.css'
+import SearchIcon from './search.svg'
 
 
-function App() {
+// fe4058bb
+// http://www.omdbapi.com/?i=tt3896198&apikey=fe4058bb
+
+const API_URL = " http://www.omdbapi.com/?apikey=fe4058bb";
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=$(title)`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
   return (
-    <h1>App</h1>
-  );
-}
+    <div className="app">
+      <h1>MovieLang</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value='superman'
+          onChange={() => {}}/>
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => {}} />
+      </div>
+
+      <div className="container">
+
+      </div>
+    </div>
+  )
+};
 
 export default App;
